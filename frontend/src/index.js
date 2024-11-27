60f7c9a835194afc4631e5cf2628ac6371ae2b00
opilane <opilane@HP-CZC2349HV0>
2024-11-27 12:08:59 +0200

    muuad index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Registration from './Registration'
-import LennujaamAdmin from './lennujaamAdmin'
+import LennujaamAdmin from './LennujaamAdmin'
+import Lennujaamkasutaja from './LennujaamKasutaja'
+import MainPage from './MainPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Registration />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/registration" element={<Registration />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/lennujaamad" element={<LennujaamAdmin />} />
+            <Route path="/lennujaamadAdmin" element={<LennujaamAdmin />} />
+            <Route path="/lennujaamad" element={<Lennujaamkasutaja />} />
         </Routes>
     </BrowserRouter>
 );
