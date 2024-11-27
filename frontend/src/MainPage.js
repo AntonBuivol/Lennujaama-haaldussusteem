b022c454b06b819ddb033d6a9fb98d1321021eb1
opilane <opilane@HP-CZC2349HV0>
2024-11-27 12:07:28 +0200

    add MainPage

---
+++ frontend/src/MainPage.js
@@ -0,0 +1,22 @@
+﻿import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./Style.css";
+
+function MainPage() {
+    return (
+        <div className="App">
+            <h1>Lennujaama Haaldussüsteem</h1>
+            <div>
+                <button><a href="http://localhost:3000/lennujaamad">Kasutaja</a></button>
+                <br/>
+                <button><a href="http://localhost:3000/lennujaamadAdmin">Admin</a></button>
+                <br/>
+                <button><a href="http://localhost:3000/login">Login</a></button>
+                <br/>
+                <button><a href="http://localhost:3000/registration">Registration</a></button>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
